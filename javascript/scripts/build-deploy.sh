2e43949a1e6a43766936fda5b8d4aa28eb5138e0
João Rosa <joaoasrosa@gmail.com>
2019-04-08 09:46:44 +0200

    Fix variables for the build script

--- javascript/scripts/build-deploy.sh
+++ javascript/scripts/build-deploy.sh
@@ -8,7 +8,7 @@ export aws_s3_bucket="baasie.cf.sac2019"
 #npm run test
 npm run build
 
-aws cloudformation package --region eu-west-1 --template-file template.yaml --s3-bucket ${aws_s3_bucket} --s3-prefix ${aws_stage} --output-template-file packaged.yaml
-aws cloudformation deploy --region eu-west-1 --template-file ./packaged.yaml --stack-name sac2019-js-${aws_stage} --parameter-overrides Stage=${aws_stage} --capabilities CAPABILITY_IAM
+aws cloudformation package --region ${AWS_REGION} --template-file template.yaml --s3-bucket ${aws_s3_bucket} --s3-prefix ${aws_stage} --output-template-file packaged.yaml
+aws cloudformation deploy --region ${AWS_REGION} --template-file ./packaged.yaml --stack-name sac2019-js-${aws_stage} --parameter-overrides Stage=${aws_stage} --capabilities CAPABILITY_IAM
 
 echo API_URL = $(aws cloudformation describe-stacks --stack-name sac2019-js-$aws_stage --query 'Stacks[0].Outputs[?OutputKey==`ParkingGarageApi`].OutputValue' --output text)
