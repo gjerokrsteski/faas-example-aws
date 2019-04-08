e5e9e97d4cb2eb199216ec63aab8e0f21d300ff6
João Rosa <joaoasrosa@gmail.com>
2019-04-08 10:32:57 +0200

    Fix stack

--- csharp/scripts/build-deploy.sh
+++ csharp/scripts/build-deploy.sh
@@ -3,7 +3,7 @@ set -e -u
 
 export AWS_REGION="eu-west-1"
 export aws_stage="kb-local"
-export aws_s3_bucket="baasie.cf.sac2019"
+export aws_s3_bucket="bassie.cf.sac2019"
 
 dotnet build
 dotnet test
@@ -11,10 +11,13 @@ dotnet test
 rm -rf artifacts
 dotnet publish src/Lambda/Lambda.csproj -o ../../artifacts/Lambda
 
-cd ./artifacts
-zip -r -X ServerlessDDD-1.0.0.zip Lambda/
+cd ./artifacts/Lambda
+zip -r -X ServerlessDDD-1.0.0.zip ./
 
 cd ..
+cd ..
+mv ./artifacts/Lambda/ServerlessDDD-1.0.0.zip ./artifacts/
+
 aws cloudformation package --region ${AWS_REGION} --template-file template.yaml --s3-bucket ${aws_s3_bucket} --s3-prefix ${aws_stage} --output-template-file packaged.yaml
 aws cloudformation deploy --region ${AWS_REGION} --template-file ./packaged.yaml --stack-name sac2019-csharp-${aws_stage} --parameter-overrides Stage=${aws_stage} --capabilities CAPABILITY_IAM
 
