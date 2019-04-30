f90ab6172e28714b8b1db5077d21fe334c8fbef4
root <root@DESKTOP-90PMEV4.localdomain>
2019-04-30 16:00:43 +0200

    test creating the CF configuration

--- scripts/build-deploy.sh
+++ scripts/build-deploy.sh
@@ -7,9 +7,10 @@ export aws_s3_bucket="nodejs-faas-exsample"
 
 npm run build
 
-aws cloudformation package --region ${AWS_REGION} --template-file template.yaml --s3-bucket ${aws_s3_bucket} --s3-prefix ${aws_stage} --output-template-file packaged.yaml
+aws cloudformation package --region ${AWS_REGION} --template-file ./template.yaml --s3-bucket ${aws_s3_bucket} --s3-prefix ${aws_stage} --output-template-file ./packaged.yaml
+
 aws cloudformation deploy --region ${AWS_REGION} --template-file ./packaged.yaml --stack-name node-js-faas-${aws_stage} --parameter-overrides Stage=${aws_stage} --capabilities CAPABILITY_IAM
 
-echo $(aws cloudformation describe-stacks --stack-name node-js-faas-$aws_stage --query 'Stacks[0].Outputs[?OutputKey==`ParkingGarageApi`].OutputValue' --output text)
+aws cloudformation describe-stacks --stack-name node-js-faas-$aws_stage --query 'Stacks[0].Outputs[?OutputKey==`ParkingGarageApi`].OutputValue' --output text
 
 
